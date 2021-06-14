488fbe55333633cce4342b33fec8a55f7418d3af
Daniel-Avo <daniel.avoyan21@gmial.com>
2021-06-14 18:34:22 +0200

    Working Url

--- app/src/main/java/com/example/raspberrypilockermobileapplication/MainActivity.java
+++ app/src/main/java/com/example/raspberrypilockermobileapplication/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.raspberrypilockermobileapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button unlock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        unlock = findViewById(R.id.unlock_button);
+
+        unlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    URL url = new URL("http://192.168.1.124:9898/light");
+                    URLConnection yc = url.openConnection();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(
+                            yc.getInputStream()));
+
+                    System.out.println(in.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
