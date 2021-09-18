e597e0785a998f71933bc24ae0772d63f8131a95
Daniel <Daniel@DESKTOP-7IMDU22>
2021-09-18 13:48:45 +0200

    Final version

--- app/src/main/java/com/example/raspberrypilockermobileapplication/MainActivity.java
+++ app/src/main/java/com/example/raspberrypilockermobileapplication/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -45,18 +46,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                    /*
+
                     URL url = new URL("http://192.168.1.110:9898/unlock");
                     URLConnection yc = url.openConnection();
                     BufferedReader in = new BufferedReader(new InputStreamReader(
                             yc.getInputStream()));
-                    */
+
                     unlock.setClickable(false);
                     Thread.sleep(3000);
                     unlock.setClickable(true);
 
-                    //System.out.println(in.readLine());
-                } catch ( InterruptedException e) {
+                    System.out.println(in.readLine());
+                } catch (InterruptedException | IOException e) {
                     e.printStackTrace();
                 }
 
